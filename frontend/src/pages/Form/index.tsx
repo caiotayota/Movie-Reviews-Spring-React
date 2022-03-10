f59f72c0eb1dfa4dd8d21a164b183c66aee8e488
caiotayota <caiotayota@live.com>
2022-03-10 00:40:38 +0000

    Pagination layout

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -11,16 +11,16 @@ function Form() {
     };
 
     return (
-        <div className="dsmovie-form-container">
-            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
-            <div className="dsmovie-card-bottom-container">
+        <div className="moviereviews-form-container">
+            <img className="moviereviews-movie-card-image" src={movie.image} alt={movie.title} />
+            <div className="moviereviews-card-bottom-container">
                 <h3>"The Witcher"</h3>
-                <form className="dsmovie-form">
-                    <div className="form-group dsmovie-form-group">
+                <form className="moviereviews-form">
+                    <div className="form-group moviereviews-form-group">
                         <label htmlFor="email">Informe seu email</label>
                         <input type="email" className="form-control" id="email" />
                     </div>
-                    <div className="form-group dsmovie-form-group">
+                    <div className="form-group moviereviews-form-group">
                         <label htmlFor="score">Informe sua avaliação</label>
                         <select className="form-control" id="score">
                             <option>1</option>
@@ -30,11 +30,11 @@ function Form() {
                             <option>5</option>
                         </select>
                     </div>
-                    <div className="dsmovie-form-btn-container">
-                        <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
+                    <div className="moviereviews-form-btn-container">
+                        <button type="submit" className="btn btn-primary moviereviews-btn">Salvar</button>
                     </div>
                 </form >
-                <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                <button className="btn btn-primary moviereviews-btn mt-3">Cancelar</button>
             </div >
         </div >
     );
